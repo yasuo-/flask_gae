41464ee42d5c6d0c645d506e61c92c42a36f112c
yasuo- <breitling1216@gmail.com>
2017-12-26 15:03:04 +0900

    template add

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import logging
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/hello')
 def hello_world():
     return 'Hello World!!!!!!!!!!!!'
 
